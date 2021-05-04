107f642e8fea5b5c209fbb3c9d879e356e3630d5
aOurfali <80325019+aOurfali@users.noreply.github.com>
2021-05-04 18:38:13 +0200

    Complete

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    
-  /*  public float speedH = 2f;
+    /*
+    public float speedH = 2f;
     public float speedV = 2f;
 
     private float yaw = 0f;
@@ -18,7 +18,8 @@ public class CameraMove : MonoBehaviour
         pitch -= speedV * Input.GetAxis("Mouse Y");
         transform.eulerAngles = new Vector3(pitch, yaw, 0f);
         
-    }*/
+    }
+    */
     
     
 }
